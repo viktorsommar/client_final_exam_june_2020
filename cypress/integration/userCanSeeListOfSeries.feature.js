7add9476481890a6cdb831a23e18f74d9de7f84c
viktor_sommar <vsommarbaillot@gmail.com>
2020-09-09 16:28:02 +0200

    Chaning ids for test to pass.

--- cypress/integration/userCanSeeListOfSeries.feature.js
+++ cypress/integration/userCanSeeListOfSeries.feature.js
@@ -11,12 +11,12 @@ describe("User can see list of shows", () => {
 
   it("on display with several titles", () => {
     cy.get("#greys-anatomy").should("be.visible")
-    cy.get("#hawaii-five").should("be.visible")
+    cy.get("#hawaii-five-0").should("be.visible")
     cy.get("#partisan").should("be.visible")
     cy.get("#seal-team").should("be.visible")
-    cy.get("#vampire-diaries").should("be.visible")
+    cy.get("#vampire-diaries-the").should("be.visible")
     cy.get("#scrubs").should("be.visible")
-    cy.get("#the-office").should("be.visible")
+    cy.get("#office-the").should("be.visible")
     cy.get("#svenska-fall").should("be.visible")
     cy.get("#areakuten").should("be.visible")
     cy.get("#american-dad").should("be.visible")
--- src/components/TvShowPage.jsx
+++ src/components/TvShowPage.jsx
@@ -1,27 +1,45 @@
 import React, { useEffect, useState } from "react";
-import { Image, Grid } from "semantic-ui-react"
+import { Image, Grid } from "semantic-ui-react";
 import axios from "axios";
 
 const TvShowPage = () => {
-  const [images, setImages] = useState([])
+  const [images, setImages] = useState([]);
 
   useEffect(() => {
-    getImages()
+    getImages();
   }, []);
 
   const getImages = async () => {
-    let response = await axios.get("https://content.viaplay.se/pc-se/serier/samtliga")
-    setImages(response.data._embedded['viaplay:blocks'][0]._embedded['viaplay:products'])
-    debugger
-  }
+    let response = await axios.get(
+      "https://content.viaplay.se/pc-se/serier/samtliga"
+    );
+    setImages(
+      response.data._embedded["viaplay:blocks"][0]._embedded["viaplay:products"]
+    );
+    debugger;
+  };
 
-  let showList = images.map((image) => (
-    <Grid>
-    <Image id="show-image" src={image.content.images.landscape.url} data-id={image.id} />
-    </Grid>
-  ))
+  let showList = images.map((image) => {
+    // <Grid>
+    //   <Grid.Column columns={4}>
+    //     <Grid.Row>
+    //       <Image
+    //         size="medium"
+    //         id="show-image"
+    //         src={image.content.images.landscape.url}
+    //         data-id={image.id}
+    //       />
+    //     </Grid.Row>
+    //   </Grid.Column>
+    // </Grid>
+    return (
+      <div className="display-show">
+        <img src={image.content.images.landscape.url} alt="" id={image.publicPath}/>
+      </div>
+    );
+  });
 
-  return <div>{showList}</div>
-}
+  return <div>{showList}</div>;
+};
 
 export default TvShowPage;
